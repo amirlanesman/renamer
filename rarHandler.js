01bddb8c48370dd33a256368ddf304f6ae2a7bcb
Amir Lanesman <lanesman@gmail.com>
2019-11-05 15:34:22 +0200

    remove missing dep

--- rarHandler.js
+++ rarHandler.js
@@ -2,7 +2,6 @@ const util = require('util')
 const unpacker = require('unpack-all')
 const unpackerListRar = util.promisify(unpacker.list)
 const unpackerExtractFiles = util.promisify(unpacker.unpack)
-const {unrar, list} = require('unrar-promise');
 
 async function listRar(file) {
   return await unpackerListRar(file, {})
